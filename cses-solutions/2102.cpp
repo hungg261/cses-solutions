d4e06c6b0714863f667f5a21b06d774e9af5e880
hungg261 <phanthanhhung.26012010@gmail.com>
2025-11-15 23:11:58 +0700

    new

--- cses-solutions/2102.cpp
+++ cses-solutions/2102.cpp
@@ -6,29 +6,27 @@ Time (YYYY-MM-DD-hh.mm.ss): 2025-11-13-22.08.45
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 1e5;
+const int MAXK = 5e5;
 int n, k;
-string matches[MAXN + 5];
+string matches[MAXK + 5];
 string S;
 
 struct AhoCorasick{
     struct Node{
         int nxt[26];
-        int completed;
-        int par, failure;
-        int ch;
-        vector<int> completes;
+        int link;
+        int occurrences;
+        vector<int> ends;
 
         Node(){
             memset(nxt, -1, sizeof nxt);
-            completed = 0;
-            par = -1;
-            failure = -1;
-            ch = -1;
+            occurrences = 0;
+            link = -1;
         }
     };
 
     vector<Node> T;
+    vector<vector<int>> adj;
     int root;
 
     AhoCorasick(){
@@ -38,61 +36,82 @@ struct AhoCorasick{
 
     void add(int idx, const string& s){
         int p = root;
-        for(char c: s){
+        for(int c: s){
             c -= 'a';
 
             if(T[p].nxt[c] == -1){
                 T[p].nxt[c] = T.size();
 
                 T.emplace_back();
-                T.back().par = p;
-                T.back().ch = c;
             }
             p = T[p].nxt[c];
         }
 
-        ++T[p].completed;
-        T[p].completes.push_back(idx);
+        T[p].ends.push_back(idx);
     }
 
     void construct(){
-        T[root].failure = root;
+        T[root].link = root;
         queue<int> que;
 
         for(int c = 0; c < 26; ++c){
-            if(T[root].nxt[c] != -1){
-                T[T[root].nxt[c]].failure = root;
-                que.push(T[root].nxt[c]);
+            int v = T[root].nxt[c];
+            if(v != -1){
+                T[v].link = root;
+                que.push(v);
+            }
+            else{
+                T[root].nxt[c] = root;
             }
         }
+
         while(!que.empty()){
-            int i = que.front();
+            int u = que.front();
             que.pop();
 
-            int j = T[T[i].par].failure;
-            while(j != root && (T[j].nxt[T[i].ch] == -1)){
-                j = T[j].failure;
-            }
-
-            if(T[j].nxt[T[i].ch] != -1 && T[j].nxt[T[i].ch] != i){
-                j = T[j].nxt[T[i].ch];
-            }
-            T[i].failure = j;
-
             for(int c = 0; c < 26; ++c){
-                if(T[i].nxt[c] != -1) que.push(T[i].nxt[c]);
+                int v = T[u].nxt[c];
+                if(v != -1){
+                    T[v].link = T[T[u].link].nxt[c];
+                    que.push(v);
+                }
+                else{
+                    T[u].nxt[c] = T[T[u].link].nxt[c];
+                }
             }
+        }
+
+        adj.resize(T.size());
+        for(int i = 1; i < T.size(); ++i){
+            adj[T[i].link].push_back(i);
         }
     }
 
-    void dfs(int u){
-        for(int c = 0; c < 26; ++c){
-            int v = T[u].nxt[c];
-            if(v != -1){
-                cerr << u << ' ' << v << " " << (char)(T[v].ch+'a') << '\n';
-                dfs(v);
+    int dfs(int u, vector<int>& res){
+        int occ = T[u].occurrences;
+        for(int v: adj[u]){
+            occ += dfs(v, res);
+        }
+
+        for(int id: T[u].ends){
+            res[id] = occ;
+        }
+
+        return occ;
+    }
+
+    void Search(const string& Text, vector<int>& res){
+        int p = root;
+        for(int c: Text){
+            c -= 'a';
+
+            p = T[p].nxt[c];
+            if(p != -1){
+                T[p].occurrences++;
             }
         }
+
+        dfs(root, res);
     }
 };
 
@@ -110,49 +129,25 @@ signed main(){
         aho.add(i, matches[i]);
     }
 
-//    aho.dfs(aho.root);
-
     aho.construct();
-//    for(int i = 0; i < aho.T.size(); ++i){
-//        cerr << i << ": " << aho.T[i].failure << '\n';
-//    }
 
-
-    int p = aho.root;
-    vector<bool> res(k, false);
-    for(int i = 0; i < n; ++i){
-        while(p > 0 && aho.T[p].nxt[S[i] - 'a'] == -1){
-            p = aho.T[p].failure;
-            for(int idx: aho.T[p].completes){
-                res[idx] = true;
-//                cerr << "\t" << idx << '\n';
-            }
-        }
-        if(aho.T[p].nxt[S[i] - 'a'] != -1) p = aho.T[p].nxt[S[i] - 'a'];
-
-//        cerr << p << '\n';
-
-        for(int idx: aho.T[p].completes){
-//            cerr << "\t" << idx << '\n';
-            res[idx] = true;
-        }
-    }
-    while(p > 0 && aho.T[p].nxt[S.back() - 'a'] == -1){
-        p = aho.T[p].failure;
-        for(int idx: aho.T[p].completes){
-            res[idx] = true;
-//            cerr << "\t" << idx << '\n';
-        }
-    }
+    vector<int> res(k, 0);
+    aho.Search(S, res);
 
     for(int i = 0; i < k; ++i){
-        cout << (res[i] ? "YES\n" : "NO\n");
+        cout << (res[i] > 0 ? "YES" : "NO") << '\n';
     }
 
     return 0;
 }
 
 /*
+aybabtu
+3
+bab
+abc
+ayba
+
 gcatcg
 4
 acc
