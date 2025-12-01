b7c35c514733c2f0a8d4dd34981fdef6a96e2447
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-01 17:25:37 +0700

    new

--- cses-solutions/1711.cpp
+++ cses-solutions/1711.cpp
@@ -14,8 +14,11 @@ struct MaxFlow{
         int cap;
         int rev;
 
+        bool original;
+        bool removed = false;
+
         EdgeTo() = default;
-        EdgeTo(int _v, int _w, int _rev): v(_v), cap(_w), rev(_rev){}
+        EdgeTo(int _v, int _w, int _rev, bool _org): v(_v), cap(_w), rev(_rev), original(_org){}
     };
 
     int n, m;
@@ -30,8 +33,8 @@ struct MaxFlow{
     }
 
     void addEdge(int a, int b, int c, bool directed = true){
-        adj[a].emplace_back(b, c, (int)adj[b].size());
-        adj[b].emplace_back(a, directed ? 0 : c, (int)adj[a].size() - 1);
+        adj[a].emplace_back(b, c, (int)adj[b].size(), true);
+        adj[b].emplace_back(a, directed ? 0 : c, (int)adj[a].size() - 1, false);
     }
 
     bool bfs_findPath(int S, int T){
@@ -59,7 +62,7 @@ struct MaxFlow{
         for(int& i = it[u]; i < (int)adj[u].size(); ++i){
             EdgeTo& e = adj[u][i];
 
-            if(e.cap > 0){
+            if(e.cap > 0 && level[u] + 1 == level[e.v]){
                 int pushed = dfs_sendFlow(e.v, min(flow, e.cap), T);
                 if(pushed > 0){
                     e.cap -= pushed;
@@ -72,6 +75,32 @@ struct MaxFlow{
         return 0;
     }
 
+    vector<pair<int, int>> minCut(int S, int T, const vector<pair<int, int>>& edges){
+        vector<bool> reachable(n + 1);
+
+        queue<int> que;
+        que.push(S);
+        reachable[S] = true;
+
+        while(!que.empty()){
+            int u = que.front(); que.pop();
+            for(const EdgeTo& e: adj[u]){
+                if(e.cap > 0 && !reachable[e.v]){
+                    reachable[e.v] = true;
+                    que.push(e.v);
+                }
+            }
+        }
+
+        vector<pair<int, int>> res;
+        for(const pair<int, int>& p: edges){
+            if(reachable[p.first] ^ reachable[p.second]){
+                res.push_back(p);
+            }
+        }
+        return res;
+    }
+
     int getFlow(int S, int T){
         int ans = 0;
         while(bfs_findPath(S, T)){
@@ -80,6 +109,14 @@ struct MaxFlow{
         }
 
         return ans;
+    }
+
+    void debug(){
+        for(int u = 1; u <= n; ++u){
+            for(const EdgeTo& e: adj[u]){
+                cerr << u << ' ' << e.v << ' ' << e.cap << '\n';
+            }
+        }
     }
 };
 
@@ -100,6 +137,28 @@ signed main(){
 
     int ans = G.getFlow(1, n);
     cout << ans << '\n';
+
+    while(ans--){
+        vector<int> path = {1};
+        int u = 1;
+        while(u != n){
+            for(MaxFlow::EdgeTo& e: G.adj[u]){
+                if(e.original && !e.removed && e.cap == 0 && G.adj[e.v][e.rev].cap > 0){
+                    path.push_back(e.v);
+
+                    u = e.v;
+                    e.removed = true;
+                    break;
+                }
+            }
+        }
+
+        cout << path.size() << '\n';
+        for(int node: path){
+            cout << node << ' ';
+        }
+        cout << '\n';
+    }
 
     return 0;
 }
