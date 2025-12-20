b655f1ba7b741c67b49fb9f48cfe1f1a90c40031
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-20 22:51:17 +0700

    new

--- cses-solutions/1736.cpp
+++ cses-solutions/1736.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
 Link: https://cses.fi/problemset/task/1736
 Code: 1736
-Time (YYYY-MM-DD-hh.mm.ss): 2025-10-04-17.59.53
+Time (YYYY-MM-DD-hh.mm.ss): 2025-12-20-21.58.52
 *******************************************************************************/
 #include<bits/stdc++.h>
 #define int long long
@@ -9,57 +9,52 @@ using namespace std;
 
 const int MAXN = 2e5;
 struct Node{
-    int value1, value2;
-    int lazy1, lazy2;
+    int delta_i = 0, delta_l = 0, lazy_i = 0, lazy_l = 0;
 
-    Node() = default;
-    Node(int _value1, int _value2, int _lazy1, int _lazy2): value1(_value1), value2(_value2), lazy1(_lazy1), lazy2(_lazy2){}
-
-    int get_sum(){
-        return value1 + value2;
+    Node operator + (const Node& other){
+        Node res;
+        res.delta_i = delta_i + other.delta_i;
+        res.delta_l = delta_l + other.delta_l;
+        return res;
     }
 
-    Node merge(const Node& other){
-        Node res;
-        res.value1 = this->value1 + other.value1;
-        res.value2 = this->value2 + other.value2;
+    void debug(){
+        cerr << delta_i << ' ' << delta_l << '\n';
+    }
 
-        res.lazy1 = 0;
-        res.lazy2 = 0;
-
-        return res;
+    int value(){
+        return delta_i + delta_l;
     }
 } nodes[MAXN * 4 + 5];
 
+inline int progsum(int l, int r){ return (l + r) * (r - l + 1) / 2; }
+
 void push(int id, int l, int r){
-    int t1 = nodes[id].lazy1, t2 = nodes[id].lazy2;
+    int &lzi = nodes[id].lazy_i, &lzl = nodes[id].lazy_l;
     int mid = (l + r) >> 1;
 
-    nodes[id << 1].value1 += t1 * (mid - l + 1);
-    nodes[id << 1].lazy1 += t1;
+    nodes[id << 1].delta_i += lzi * progsum(l, mid);
+    nodes[id << 1].delta_l -= lzl * (mid - l + 1);
+    nodes[id << 1].lazy_i += lzi;
+    nodes[id << 1].lazy_l += lzl;
 
-    nodes[id << 1].value2 += t2 * (mid - l + 1) * (mid - l + 2) / 2;
-    nodes[id << 1].lazy2 += t2;
+    nodes[id << 1 | 1].delta_i += lzi * progsum(mid + 1, r);
+    nodes[id << 1 | 1].delta_l -= lzl * (r - mid);
+    nodes[id << 1 | 1].lazy_i += lzi;
+    nodes[id << 1 | 1].lazy_l += lzl;
 
-    nodes[id << 1 | 1].value1 += t1 * (r - mid);
-    nodes[id << 1 | 1].lazy1 += t1;
-
-    nodes[id << 1 | 1].value2 += t2 * (r - mid) * (r - mid + 1) / 2;
-    nodes[id << 1 | 1].lazy2 += t2;
-
-    nodes[id].lazy1 = 0;
-    nodes[id].lazy2 = 0;
+    lzi = 0;
+    lzl = 0;
 }
 
 void update(int id, int l, int r, int u, int v){
-    if(v < l || r < u) return;
+    if(r < u || v < l) return;
     if(u <= l && r <= v){
-        nodes[id].value1 += (l - u) * (r - l + 1);
-        nodes[id].lazy1 += l - u;
+        nodes[id].delta_i += progsum(l, r);
+        nodes[id].delta_l -= (u - 1) * (r - l + 1);
 
-        nodes[id].value2 += (r - l + 1) * (r - l + 2) / 2;
-        nodes[id].lazy2++;
-
+        nodes[id].lazy_i++;
+        nodes[id].lazy_l += u - 1;
         return;
     }
 
@@ -67,31 +62,32 @@ void update(int id, int l, int r, int u, int v){
 
     int mid = (l + r) >> 1;
     update(id << 1, l, mid, u, v);
-    update(id << 1 | 1, mid + 1, r, u , v);
-    nodes[id] = nodes[id << 1].merge(nodes[id << 1 | 1]);
+    update(id << 1 | 1, mid + 1, r, u, v);
+    nodes[id] = nodes[id << 1] + nodes[id << 1 | 1];
 }
 
 Node get(int id, int l, int r, int u, int v){
-    if(v < l || r < u) return Node(0, 0, 0, 0);
+    if(r < u || v < l) return Node();
     if(u <= l && r <= v) return nodes[id];
 
     push(id, l, r);
 
     int mid = (l + r) >> 1;
-    return get(id << 1, l, mid, u, v).merge(get(id << 1 | 1, mid + 1, r, u, v));
+    return get(id << 1, l, mid, u, v) + get(id << 1 | 1, mid + 1, r, u, v);
 }
+
+int n, q;
+int arr[MAXN + 5], pre[MAXN + 5];
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-    int n, q;
+    //freopen("1736.INP","r",stdin);
+    //freopen("1736.OUT","w",stdout);
+
     cin >> n >> q;
-
-    vector<int> pre(n + 1);
-    pre[0] = 0;
-
     for(int i = 1; i <= n; ++i){
-        int cur; cin >> cur;
-        pre[i] = pre[i - 1] + cur;
+        cin >> arr[i];
+        pre[i] = pre[i - 1] + arr[i];
     }
 
     while(q--){
@@ -102,7 +98,7 @@ signed main(){
             update(1, 1, n, a, b);
         }
         else{
-            cout << pre[b] - pre[a - 1] + get(1, 1, n, a, b).get_sum() << '\n';
+            cout << get(1, 1, n, a, b).value() + (pre[b] - pre[a - 1]) << '\n';
         }
     }
 
