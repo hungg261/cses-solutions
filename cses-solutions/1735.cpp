1e8bc41527f0149aff863ba2e985ad87517754ca
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-21 23:33:57 +0700

    unfinished

--- cses-solutions/1735.cpp
+++ cses-solutions/1735.cpp
@@ -1,126 +1,129 @@
 /******************************************************************************
 Link: https://cses.fi/problemset/task/1735
 Code: 1735
-Time (YYYY-MM-DD-hh.mm.ss): 2025-10-06-22.15.08
+Time (YYYY-MM-DD-hh.mm.ss): 2025-12-21-20.15.45
 *******************************************************************************/
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
 
 const int MAXN = 2e5;
-struct Node{
-    int value;
-    int lazy1, lazy2;
-
-    Node(): value(0), lazy1(0), lazy2(0){}
-
-    Node merge(const Node& other){
-        Node res;
-        res.value = this->value + other.value;
-        res.lazy1 = res.lazy2 = 0;
-
-        return res;
-    }
-};
-
-int arr[MAXN + 5], n;
-Node nodes[MAXN * 4 + 1];
+int n, q, arr[MAXN + 5];
+int nodes[MAXN * 4 + 5], lazyInc[MAXN * 4 + 5], lazySet[MAXN * 4 + 5], pending[MAXN * 4 + 5];
 
 void build(int id, int l, int r){
     if(l == r){
-        nodes[id].value = arr[l];
+        nodes[id] = arr[l];
         return;
     }
 
     int mid = (l + r) >> 1;
     build(id << 1, l, mid);
     build(id << 1 | 1, mid + 1, r);
-    nodes[id] = nodes[id << 1].merge(nodes[id << 1 | 1]);
+    nodes[id] = nodes[id << 1] + nodes[id << 1 | 1];
 }
 
-void push_set(int id){
-    int t = nodes[id].lazy2;
-    if(t == 0) return;
+void downInc(int id, int l, int r){
+    int& lz = lazyInc[id];
+    if(lz == 0) return;
+    int mid = (l + r) >> 1;
 
-    nodes[id << 1].value = t;
-    nodes[id << 1].lazy2 = t;
-    nodes[id << 1].lazy1 = 0;
+    nodes[id << 1] += lz * (mid - l + 1);
+    lazyInc[id << 1] += lz;
 
-    nodes[id << 1 | 1].value = t;
-    nodes[id << 1 | 1].lazy2= t;
-    nodes[id << 1 | 1].lazy1 = 0;
+    nodes[id << 1 | 1] += lz * (r - mid);
+    lazyInc[id << 1 | 1] += lz;
 
-    nodes[id].lazy2 = 0;
+    lz = 0;
+    pending[id] = 0;
+    pending[id << 1] = pending[id << 1 | 1] = 1;
 }
 
-void push_inc(int id){
-    push_set(id);
+void downSet(int id, int l, int r){
+    int& lz = lazySet[id];
+    if(lz == 0) return;
+    int mid = (l + r) >> 1;
 
-    int t = nodes[id].lazy1;
+    nodes[id << 1] = lz * (mid - l + 1);
+    lazySet[id << 1] = lz;
 
-    nodes[id << 1].value += t;
-    nodes[id << 1].lazy1 += t;
-    nodes[id << 1].lazy2 += t;
+    nodes[id << 1 | 1] = lz * (r - mid);
+    lazySet[id << 1 | 1] = lz;
 
-    nodes[id << 1 | 1].value += t;
-    nodes[id << 1 | 1].lazy1 += t;
-    nodes[id << 1 | 1].lazy2 += t;
-
-    nodes[id].lazy1 = 0;
+    lz = 0;
+    pending[id] = 0;
+    pending[id << 1] = pending[id << 1 | 1] = 2;
 }
 
-void increase(int id, int l, int r, int u, int v, int val){
-    if(v < l || r < u) return;
+void Increase(int id, int l, int r, int u, int v, int x){
+    if(r < u || v < l) return;
     if(u <= l && r <= v){
-        nodes[id].value += val;
-        nodes[id].lazy1 += val;
-        nodes[id].lazy2 += val;
+        nodes[id] += x * (r - l + 1);
+        lazyInc[id] += x;
+
+        pending[id] = 1;
         return;
     }
 
-    push_inc(id);
+    downSet(id, l, r);
+    downInc(id, l, r);
 
     int mid = (l + r) >> 1;
-    increase(id << 1, l, mid, u, v, val);
-    increase(id << 1 | 1, mid + 1, r, u, v, val);
-    nodes[id] = nodes[id << 1].merge(nodes[id << 1 | 1]);
+    Increase(id << 1, l, mid, u, v, x);
+    Increase(id << 1 | 1, mid + 1, r, u, v, x);
+    nodes[id] = nodes[id << 1] + nodes[id << 1 | 1];
 }
 
-void update(int id, int l, int r, int u, int v, int val){
-    if(v < l || r < u) return;
+void Set(int id, int l, int r, int u, int v, int x){
+    if(r < u || v < l) return;
     if(u <= l && r <= v){
-        nodes[id].value = val;
-        nodes[id].lazy2 = val;
-        nodes[id].lazy1 = 0;
+        nodes[id] = x * (r - l + 1);
+        lazySet[id] = x;
+
+        pending[id] = 2;
+//        cerr << "pending: " << id << ' ' << l << ' ' << r << ' ' << u << ' ' << v << ' ' << x << " | " << pending[3] << '\n';
         return;
     }
 
-    push_set(id);
+    downInc(id, l, r);
+    downSet(id, l, r);
 
     int mid = (l + r) >> 1;
-    update(id << 1, l, mid, u, v, val);
-    update(id << 1 | 1, mid + 1, r, u, v, val);
-    nodes[id] = nodes[id << 1].merge(nodes[id << 1 | 1]);
+    Set(id << 1, l, mid, u, v, x);
+    Set(id << 1 | 1, mid + 1, r, u, v, x);
+    nodes[id] = nodes[id << 1] + nodes[id << 1 | 1];
 }
 
-Node get(int id, int l, int r, int u, int v){
-    if(v < l || r < u) return Node();
+int get(int id, int l, int r, int u, int v){
+    if(r < u || v < l) return 0;
     if(u <= l && r <= v) return nodes[id];
 
+    if(pending[id] == 1){
+        downSet(id, l, r);
+        downInc(id, l, r);
+    }
+    else if(pending[id] == 2){
+        downInc(id, l, r);
+        downSet(id, l, r);
+    }
+
     int mid = (l + r) >> 1;
-    return get(id << 1, l, mid, u, v).merge(get(id << 1 | 1, mid + 1, r, u, v));
+    return get(id << 1, l, mid, u, v) + get(id << 1 | 1, mid + 1, r, u, v);
 }
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     //freopen("1735.INP","r",stdin);
     //freopen("1735.OUT","w",stdout);
-    int q;
+
+//    Increase(1, 1, 2, 1, 2, 5);
+//    Set(1, 1, 2, 1, 2, 2);
+//    cout << get(1, 1, 2, 1, 2) << '\n';
+
     cin >> n >> q;
     for(int i = 1; i <= n; ++i){
         cin >> arr[i];
     }
-
     build(1, 1, n);
 
     while(q--){
@@ -131,22 +134,21 @@ signed main(){
             int a, b, x;
             cin >> a >> b >> x;
 
-            increase(1, 1, n, a, b, x);
+            Increase(1, 1, n, a, b, x);
         }
         else if(type == 2){
             int a, b, x;
             cin >> a >> b >> x;
 
-            update(1, 1, n, a, b, x);
+            Set(1, 1, n, a, b, x);
         }
         else{
             int a, b;
             cin >> a >> b;
 
-            cout << get(1, 1, n, a, b).value << '\n';
+            cout << get(1, 1, n, a, b) << '\n';
         }
     }
-
 
     return 0;
 }
